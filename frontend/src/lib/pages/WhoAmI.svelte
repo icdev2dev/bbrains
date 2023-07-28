2f1a9646b3ab42f477e3e8162070afaf8b191640
icdev2dev <icdev2dev@gmail.com>
2023-07-28 16:32:28 -0700

    mvp2

--- frontend/src/lib/pages/WhoAmI.svelte
+++ frontend/src/lib/pages/WhoAmI.svelte
@@ -1,1 +1,28 @@
-Who Am I?
+<script>
+    import { writable } from 'svelte/store';
+     
+    import { mypersonasYaml } from '../../dataservices';
+    import { whoamiYaml } from '../../dataservices';
+
+    let whoami = $whoamiYaml.whoami
+    let mypersonas = $mypersonasYaml.personas
+    let mypersonasStore = writable(mypersonas);
+
+    
+</script>
+<h1> Who Am I </h1>
+{whoami}
+<h2> My Personas </h2>
+<table>
+    <tr>
+        <th> Persona</th>
+        <th> Persona Description</th>
+    </tr>
+    {#each $mypersonasStore as mypersona }
+        <tr>
+            <td> {mypersona.Persona}</td>
+            <td> {mypersona.Description}</td>
+        </tr>
+        
+    {/each}
+</table>
